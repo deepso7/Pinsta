da829e63d08e184449c009063a4cc656916a43d2
Deepanshu <deeps.o7o8.y@gmail.com>
2021-11-04 01:38:12 +0530

    add miniprofile, suggestions

--- components/Posts/Post.tsx
+++ components/Posts/Post.tsx
@@ -1,7 +1,68 @@
-const Post = () => {
+import { FC } from "react";
+import {
+  BookmarkIcon,
+  ChatIcon,
+  DotsHorizontalIcon,
+  EmojiHappyIcon,
+  HeartIcon,
+  PaperAirplaneIcon,
+} from "@heroicons/react/outline";
+import { HeartIcon as HeartIconFilled } from "@heroicons/react/solid";
+
+interface postProps {
+  id: string;
+  username: string;
+  userImg: string;
+  img: string;
+  caption: string;
+}
+
+const Post: FC<postProps> = ({ id, username, userImg, img, caption }) => {
   return (
-    <div>
-      <h1>I am post</h1>
+    <div className="bg-white my-7 border rounded-sm">
+      {/* Header */}
+      <div className="flex items-center p-5">
+        <img
+          src={userImg}
+          className="rounded-full h-12 w-12 object-contain border p-1 mr-3"
+          alt=""
+        />
+        <p className="flex-1 font-bold">{username}</p>
+        <DotsHorizontalIcon className="h-5" />
+      </div>
+
+      {/* img */}
+      <img src={img} className="w-full" alt="" />
+
+      {/* button */}
+      <div className="flex justify-between px-4 pt-4">
+        <div className="flex space-x-4">
+          <HeartIcon className="btn" />
+          <ChatIcon className="btn" />
+          <PaperAirplaneIcon className="btn" />
+        </div>
+
+        <BookmarkIcon className="btn" />
+      </div>
+
+      {/* caption */}
+      <p className="p-5 truncate">
+        <span className="font-bold mr-1">{username}</span>
+        {caption}
+      </p>
+
+      {/* commnets */}
+
+      {/* input box */}
+      <form className="flex items-center p-4">
+        <EmojiHappyIcon className="h-7" />
+        <input
+          type="text"
+          placeholder="Add a comment..."
+          className="border-none flex-1 focus:ring-0 outline-none"
+        />
+        <button className="font-semibold text-blue-400">Post</button>
+      </form>
     </div>
   );
 };
--- components/Posts/index.tsx
+++ components/Posts/index.tsx
@@ -1,19 +1,35 @@
 import Post from "./Post";
 
-const DUMMY_DATA = [
+const posts = [
   {
     id: "123",
     username: "deepso",
+    userImg: "https://links.papareact.com/3ke",
+    img: "https://links.papareact.com/3ke",
+    caption: "This is dope",
+  },
+  {
+    id: "124",
+    username: "deepso",
+    userImg: "https://links.papareact.com/3ke",
+    img: "https://links.papareact.com/3ke",
+    caption: "This is dope",
   },
 ];
 
 const Posts = () => {
   return (
     <div>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      {posts.map((post) => (
+        <Post
+          key={post.id}
+          id={post.id}
+          username={post.username}
+          userImg={post.userImg}
+          img={post.img}
+          caption={post.caption}
+        />
+      ))}
     </div>
   );
 };
